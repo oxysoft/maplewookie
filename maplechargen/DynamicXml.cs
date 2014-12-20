e8b18fdf272a67e56d78852bf06fc8732b967e2a
oxysoft <teoxysoft@gmail.com>
2014-12-20 14:31:57 -0500

    initial commit

---
+++ maplechargen/DynamicXml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Dynamic;
+using System.Xml.Linq;
+using System.Collections;
+
+namespace maplechargen {
+	public class DynamicXml : DynamicObject, IEnumerable {
+		private readonly List<XElement> _elements;
+
+		public DynamicXml(string text) {
+			var doc = XDocument.Parse(text);
+			_elements = new List<XElement> { doc.Root };
+		}
+
+		protected DynamicXml(XElement element) {
+			_elements = new List<XElement> { element };
+		}
+
+		protected DynamicXml(IEnumerable<XElement> elements) {
+			_elements = new List<XElement>(elements);
+		}
+
+		public override bool TryGetMember(GetMemberBinder binder, out object result) {
+			result = null;
+			if (binder.Name == "Value")
+				result = _elements[0].Value;
+			else if (binder.Name == "Count")
+				result = _elements.Count;
+			else {
+				var attr = _elements[0].Attribute(XName.Get(binder.Name));
+				if (attr != null)
+					result = attr;
+				else {
+					var items = _elements.Descendants(XName.Get(binder.Name));
+					if (items == null || !items.Any()) return false;
+					result = new DynamicXml(items);
+				}
+			}
+			return true;
+		}
+
+		public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object result) {
+			int ndx = (int)indexes[0];
+			result = new DynamicXml(_elements[ndx]);
+			return true;
+		}
+
+		public IEnumerator GetEnumerator() {
+			return _elements.Select(element => new DynamicXml(element)).GetEnumerator();
+		}
+	}
+}
---
+++ maplechargen/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace maplechargen {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.button1 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.button1.Location = new System.Drawing.Point(0, 238);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(303, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "generate";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.onGenerateClicked);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(303, 238);
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(303, 261);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
+
